80e7a1a7ce88b51eaa8b6044042f40b9ebdbfa7f
Гоша Браженко <goshabrajenko@gmail.com>
2022-01-11 11:32:38 +0300

    create funcs for enemies spawn

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import pygame
 import os
 import random
 
-BULLET_V = 5
-SHOOTING_ENEMY_WALK_RANGE = 100
+BULLET_V = 3
+SHOOTING_ENEMY_POSITIONS = [(100, 100), (100, 900), (100, 500), (900, 100), (900, 900), (900, 500), (500, 100),
+                            (500, 900), (250, 100), (750, 100), (250, 900), (750, 900), (100, 250), (100, 750),
+                            (900, 250), (900, 750)]
+print(SHOOTING_ENEMY_POSITIONS)
+SHOOTING_SPEED = 2
+ENEMY_SPEED = 1
 
 
 def load_image(name, colorkey=(255, 255, 255)):
@@ -34,7 +39,6 @@ def count_move(pos, dest):
 pygame.init()
 FPS = 60
 SIZE = WIDTH, HEIGHT = 1000, 1000
-GRAVITY = 1
 screen_rect = 0, 0, *SIZE
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Марио?')
@@ -81,6 +85,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(all_sprites, draw_first)
         self.image = load_image('mario.png')
         self.rect = self.image.get_rect()
+        self.rect.center = (500, 500)
 
     def update(self, *args):
         self.move = [0, 0]
@@ -106,31 +111,20 @@ class Player(pygame.sprite.Sprite):
         if pygame.sprite.spritecollideany(self, enemies):
             self.kill()
 
+    def get_pos(self):
+        return self.rect.center
 
-def get_pos(self):
-    return self.rect.center
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos):
         super().__init__(enemies, all_sprites)
         self.image = load_image('star.png')
         self.rect = self.image.get_rect()
-        self.rect.center = (400, 400)
+        self.rect.center = pos
 
     def update(self, *args):
         if player.alive():
-            target_x, target_y = player.get_pos()
-            move = [0, 0]
-            if self.rect.x > target_x:
-                move[0] -= 1
-            elif self.rect.x < target_x:
-                move[0] += 1
-
-            if self.rect.y > target_y:
-                move[1] -= 1
-            elif self.rect.y < target_y:
-                move[1] += 1
-
+            move = count_move(self.rect.center, player.get_pos())
             self.rect = self.rect.move(*move)
 
 
@@ -150,62 +144,107 @@ class Bullet(pygame.sprite.Sprite):
             self.rect = self.rect.move(*self.move)
 
 
+def find_closest_point(pos, points):
+    minn = 99999
+    ans = points[0]
+    for point in points:
+        dist = math.sqrt((pos[0] - point[0]) ** 2 + (pos[1] - point[1]) ** 2)
+        if minn > dist:
+            ans = point
+            minn = dist
+
+    return ans
+
+
 class ShootingEnemy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos):
         super().__init__(enemies, all_sprites)
         self.image = load_image('mario.png')
         self.rect = self.image.get_rect()
-        self.rect.center = (419, 1000)
+        self.rect.center = pos
         self.counter = 0
 
         self.moving = True
-        self.dest = [0, 0]
-        if self.rect.centerx > player.rect.centerx:
-            self.dest[0] = player.rect.centerx + SHOOTING_ENEMY_WALK_RANGE
-        else:
-            self.dest[0] = player.rect.centerx - SHOOTING_ENEMY_WALK_RANGE
-
-        if self.rect.centery > player.rect.centery:
-            self.dest[1] = player.rect.centery + SHOOTING_ENEMY_WALK_RANGE
-        else:
-            self.dest[1] = player.rect.centery - SHOOTING_ENEMY_WALK_RANGE
+        self.dest = find_closest_point(self.rect.center, SHOOTING_ENEMY_POSITIONS)
 
     def update(self, *args):
         if player.alive():
             if self.moving:
                 move = count_move(self.rect.center, self.dest)
                 self.rect = self.rect.move(move)
+                if self.rect.center == self.dest:
+                    self.moving = False
             else:
                 self.counter += clock.get_time()
                 if self.counter > 500:
-                    # self.shoot()
+                    self.shoot()
                     self.counter = 0
 
     def shoot(self):
         dx = player.rect.centerx - self.rect.centerx
         dy = player.rect.centery - self.rect.centery
-        x_speed = BULLET_V / (math.sqrt(1 + ((dy ** 2) / (dx ** 2))))
-        y_speed = BULLET_V / (math.sqrt(1 + ((dx ** 2) / (dy ** 2))))
-        if dx < 0:
-            x_speed *= -1
-        if dy < 0:
-            y_speed *= -1
-        move = [x_speed, y_speed]
-        bullet = Bullet(self.rect.center, move)
+        try:
+            x_speed = BULLET_V / (math.sqrt(1 + ((dy ** 2) / (dx ** 2))))
+            y_speed = BULLET_V / (math.sqrt(1 + ((dx ** 2) / (dy ** 2))))
+        except ZeroDivisionError:
+            if dx == 0:
+                x_speed = 0
+                y_speed = BULLET_V / (math.sqrt(1 + ((dx ** 2) / (dy ** 2))))
+            elif dy == 0:
+                x_speed = BULLET_V / (math.sqrt(1 + ((dy ** 2) / (dx ** 2))))
+                y_speed = 0
+        finally:
+            if dx < 0:
+                x_speed *= -1
+            if dy < 0:
+                y_speed *= -1
+            move = [x_speed, y_speed]
+            bullet = Bullet(self.rect.center, move)
 
 
 player = Player()
 all_sprites.add(player)
 
 
-# enemy = ShootingEnemy()
-# enemies.add(enemy)
+def spawn_enemy(pos: (int, int), shooting=False):
+    if shooting:
+        enemy = ShootingEnemy(pos)
+    else:
+        enemy = Enemy(pos)
+    enemies.add(enemy)
+
+
+def generate_random_pos(shooting: bool) -> (int, int):
+    if shooting:
+        positions = [list(range(0, 101)) + list(range(900, 1001)), list(range(0, 1001))]
+        random.shuffle(positions)
+        x = random.choice(positions[0])
+        y = random.choice(positions[1])
+        return x, y
+
+
+def random_spawn(n: int, shooting=False) -> None:
+    positions = [generate_random_pos(shooting) for _ in range(n)]
+    for pos in positions:
+        spawn_enemy(pos, shooting)
+
+
+random_spawn(5, shooting=True)
+
 
 def main():
+    spawn_delay = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
+
+        spawn_delay += 1
+        if spawn_delay == 100:
+            pass
+
+        if spawn_delay == 1000:
+            spawn_delay = 0
 
         all_sprites.update()
         screen.fill(pygame.Color('black'))
