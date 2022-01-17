8d0b6fc66a287b0f1d1c09630c19670333676118
Гоша Браженко <goshabrajenko@gmail.com>
2022-01-17 17:55:05 +0300

    fixed results write problem

--- results.py
+++ results.py
@@ -4,7 +4,17 @@ from operator import itemgetter
 HEADERS = ['nickname', 'result']
 
 
+def check_file():
+    try:
+        open('results.csv').close()
+    except FileNotFoundError:
+        with open('results.csv', encoding="utf8", mode='w') as res:
+            writer = csv.DictWriter(res, delimiter=';', fieldnames=HEADERS, quotechar='"')
+            writer.writeheader()
+
+
 def write_new(nick: str, result: int) -> None:
+    check_file()
     with open('results.csv', encoding="utf8", mode='a') as res:
         writer = csv.DictWriter(res, delimiter=';', fieldnames=HEADERS, quotechar='"')
         writer.writerow({'nickname': nick, 'result': result})
@@ -12,6 +22,7 @@ def write_new(nick: str, result: int) -> None:
 
 
 def get_best() -> (str, int):
+    check_file()
     with open('results.csv', encoding="utf8") as res:
         reader = csv.DictReader(res, delimiter=';', quotechar='"')
         try:
@@ -22,6 +33,7 @@ def get_best() -> (str, int):
 
 
 def get_last() -> (str, int):
+    check_file()
     with open('results.csv', encoding="utf8") as res:
         reader = csv.DictReader(res, delimiter=';', quotechar='"')
         return dict_to_tuple(list(reader)[-1])
